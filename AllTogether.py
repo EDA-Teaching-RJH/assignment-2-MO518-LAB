060759f7e3580621c9e67431638dfc8d8b3ae829
MO518-LAB <mo518@kent.ac.uk>
2024-10-27 10:43:55 +0000

    Update

--- AllTogether.py
+++ AllTogether.py
@@ -1,1 +1,14 @@
 ### Part Four -- your code goes here. 
+import random
+
+my_list = [random.randint(1,100) for _ in range(10)]
+print("Old List", my_list)
+
+i = 0
+while i < len(my_list):
+    if my_list(i) % 2 == 0:
+        my_list.pop(i)
+    else:
+        i += 1
+
+print("New List", my_list)
